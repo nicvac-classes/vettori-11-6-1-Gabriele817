e6b3b64e8d7781f5e89a6e88d6f1cfd107857087
Gabriele817 <gabriele.grassi@marconibari.edu.it>
2025-02-17 19:43:02 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,65 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+import java.util.*;
+import java.lang.Math;
 
-//Import di Classi Java necessarie al funzionamento del programma
-import java.util.Scanner;
+class Program {
+    public static void main(String[] args) {
+    }
+    
+    public static int prendiPrestito(String[] biblioteca, String[] prestiti, int dimensione, String libro, int count) {
+        int elemento, i;
+        boolean trovato;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+        trovato = false;
+        for (i = 0; i <= dimensione - 1; i++) {
+            if (biblioteca[i].equals(libro)) {
+                elemento = i;
+                trovato = true;
+                inserisciElemento(biblioteca, prestiti, dimensione, elemento, biblioteca[i], count);
+                dimensione = eliminaElemento(biblioteca, dimensione, elemento);
+                System.out.println("il libro" + biblioteca[i] + "e stato preso in prestito");
+            }
+        }
+        if (!trovato) {
+            System.out.println("libro non presente in libreria");
+        }
+        
+        return dimensione;
+    }
+    
+    public static int restituiciPrestito(int[] biblioteca, int[] prestiti, int dimensione, int elemento) {
+        if (!(elemtento < 0) && !(elemento > dimensione)) {
+            int dimensione;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+            dimensione = elimanaElemento(prestiti, dimensione, elemento);
+            inserisciElemento(biblioteca, dimensione, prestiti[elemento], elemento);
+        } else {
+            System.out.println("Questo libro non è mai stato preso in prestito");
+        }
+        
+        return dimensione;
+    }
+    
+    public static void visualizzaBiblioteca(String vettore, int dimensione) {
+        int i;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        for (i = 0; i <= dimensione - 1; i++) {
+            if (dimensione > 0) {
+                System.out.println(Integer.toString(i + 1) + " : " + vettore[i]);
+            } else {
+                System.out.println("La biblioteca è vuota i libri sono stati presi tutti in prestito");
+            }
+        }
+    }
+    
+    public static void visualizzaPrestiti(String[] vettore, int dimensione) {
+        int i;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        for (i = 0; i <= dimensione - 1; i++) {
+            if (dimensione > 0) {
+                System.out.println("");
+            } else {
+                System.out.println("la biblioteca è piena nessun libro è stato preso in prestito");
+            }
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
